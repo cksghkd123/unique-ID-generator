28b1a7ba7155f268f0e8ee40025fa772b682171d
chanjun <cksghkd1234@naver.com>
2025-06-24 00:14:32 +0900

    rest api로 수정

--- src/main/kotlin/com/example/ex/SimpleController.kt
+++ src/main/kotlin/com/example/ex/SimpleController.kt
@@ -3,12 +3,13 @@ package com.example.ex
 import SnowflakeGenerator
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class SimpleController(private val snowflakeGenerator: SnowflakeGenerator) {
     @GetMapping("/generate-id")
+    @ResponseBody
     fun generateId(): Long {
-        return val id = snowflakeGenerator.nextId()
+        return snowflakeGenerator.nextId()
     }
-
 }
