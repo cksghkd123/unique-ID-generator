1fe1e88cbe5defb314d20c85ac4315825e0228c7
chanjun <cksghkd1234@naver.com>
2025-06-26 01:47:53 +0900

    test 코드 추가

--- src/test/kotlin/com/example/ex/SnowflakeGeneratorTest.kt
+++ src/test/kotlin/com/example/ex/SnowflakeGeneratorTest.kt
@@ -1,27 +1,29 @@
 package com.example.ex
 
 import SnowflakeGenerator
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 
 class SnowflakeGeneratorTest {
-//    @BeforeEach
-//    fun setUp() {
-//        TODO("Not yet implemented")
-//    }
-//
-//    @AfterEach
-//    fun tearDown() {
-//        TODO("Not yet implemented")
-//    }
+    @Test
+    fun `단일서버 단일프로세스 싱글스레드`() {
+        val generator = SnowflakeGenerator(1, 0)
+        val requestPerThread = 10000
+        val result = ConcurrentHashMap.newKeySet<Long>()
+
+        repeat(requestPerThread) {
+            result.add(generator.nextId())
+        }
+
+        assertEquals(requestPerThread, result.size, "아이디가 중복되어 생성되었음")
+
+    }
 
     @Test
-    fun `동시성 환경에서 ID 생성 테스트`() {
+    fun `단일서버 단일프로세스 멀티스레드`() {
         val generator = SnowflakeGenerator(1, 0)
         val threadCount = 100
         val requestPerThread = 1000
@@ -42,7 +44,7 @@ class SnowflakeGeneratorTest {
         executor.shutdown()
 
         val total = threadCount * requestPerThread
-        assertEquals(total, result.size, "중복 없이 ID가 생성되어야 합니다.")
+        assertEquals(total, result.size, "아이디가 중복되어 생성되었음")
 
 
     }
